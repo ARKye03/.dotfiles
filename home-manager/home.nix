b33d9c570f8c98681e7ddfcd955b889b6d1f2c61
ARKye03 <rafa2003sanchez@gmail.com>
2024-04-01 19:02:53 -0400

    Add warp-terminal package

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -56,6 +56,7 @@ in
     pkgs.tgpt
     pkgs.cava
     pkgs.btop
+    pkgs.warp-terminal
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
