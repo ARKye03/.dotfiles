ea848d9bfd1889b09271bde306b7a431ba7cbd7c
ARKye03 <rafa2003sanchez@gmail.com>
2024-04-01 19:02:48 -0400

    Add shell aliases and packages to home.nix

---
+++ configuration.nix
@@ -0,0 +1,160 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [
+      # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.supportedFilesystems = [ "ntfs" ];
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+  #ZSH
+  programs.zsh.enable = true;
+  users.defaultUserShell = pkgs.zsh;
+
+  # Set your time zone.
+  time.timeZone = "America/Havana";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "es_CU";
+    LC_IDENTIFICATION = "es_CU";
+    LC_MEASUREMENT = "es_CU";
+    LC_MONETARY = "es_CU";
+    LC_NAME = "es_CU";
+    LC_NUMERIC = "es_CU";
+    LC_PAPER = "es_CU";
+    LC_TELEPHONE = "es_CU";
+    LC_TIME = "es_CU";
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "us";
+    xkbVariant = "";
+  };
+
+  # Configure console keymap
+  console.keyMap = "us-acentos";
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.nixarkye = {
+    isNormalUser = true;
+    description = "nixarkye";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+      firefox
+    ];
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  environment.sessionVariables = {
+    NIXOS_OZONE_WL = "1";
+  };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    vim
+    wget
+    htop
+    dotnet-sdk_8
+    neovim
+    docker
+    fastfetch
+    riseup-vpn
+    networkmanagerapplet
+    git
+    nodejs
+    ntfs3g
+    rustup
+    gnumake
+    gcc
+    ffmpeg
+    nmap
+  ];
+
+  fonts.packages = with pkgs; [
+    fira-code
+    fira-code-symbols
+    fira-code-nerdfont
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.11"; # Did you read the comment?
+}
---
+++ home-manager/home.nix
@@ -0,0 +1,125 @@
+{ config, pkgs, ... }:
+let
+  shellAliases = {
+    ll = "ls -l";
+    nixsu = "sudo nixos-rebuild switch";
+    homsu = "home-manager switch";
+    lvnix = "sudo lvim /etc/nixos/configuration.nix";
+    lvhom = "lvim /home/nixarkye/.config/home-manager/home.nix";
+    gc = "git clone";
+    codenix = "code /home/nixarkye/.dotfiles";
+    #.Net Aliases
+    dn = "dotnet new";
+    dr = "dotnet run";
+    dt = "dotnet test";
+    dw = "dotnet watch";
+    dwr = "dotnet watch run";
+    dwt = "dotnet watch test";
+    ds = "dotnet sln";
+    da = "dotnet add";
+    dp = "dotnet pack";
+    dng = "dotnet nuget";
+    db = "dotnet build";
+  };
+in
+{
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "nixarkye";
+  home.homeDirectory = "/home/nixarkye";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "23.11"; # Please read the comment before changing.
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = [
+    pkgs.protonvpn-cli
+    pkgs.microsoft-edge
+    pkgs.discord
+    pkgs.telegram-desktop
+    pkgs.gnome-browser-connector
+    pkgs.fastfetch
+    pkgs.geogebra
+    pkgs.celluloid
+    pkgs.libreoffice
+    pkgs.gh
+    pkgs.kitty
+    pkgs.uget
+    pkgs.ocs-url
+    pkgs.tgpt
+    pkgs.cava
+    pkgs.btop
+
+    # # It is sometimes useful to fine-tune packages, for example, by applying
+    # # overrides. You can do that directly here, just don't forget the
+    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+    # # fonts?
+    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+
+    # # You can also create simple shell scripts directly inside your
+    # # configuration. For example, this adds a command 'my-hello' to your
+    # # environment:
+    # (pkgs.writeShellScriptBin "my-hello" ''
+    #   echo "Hello, ${config.home.username}!"
+    # '')
+  ];
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  # Home Manager can also manage your environment variables through
+  # 'home.sessionVariables'. If you don't want to manage your shell through Home
+  # Manager then you have to manually source 'hm-session-vars.sh' located at
+  # either
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/nixarkye/etc/profile.d/hm-session-vars.sh
+  #
+  home.sessionPath = [
+    "${config.home.homeDirectory}/.local/bin"
+    "${config.home.homeDirectory}/.cargo/bin"
+  ];
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  programs.zsh = {
+    enable = true;
+    syntaxHighlighting.enable = true;
+    enableAutosuggestions = true;
+    enableCompletion = true;
+    shellAliases = shellAliases;
+    history = {
+      ignoreDups = true;
+      size = 10000;
+      path = "${config.xdg.dataHome}/zsh/history";
+    };
+    oh-my-zsh = {
+      enable = true;
+      theme = "robbyrussell";
+      plugins = [ "git" ];
+    };
+  };
+}
