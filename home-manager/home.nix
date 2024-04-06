b300e68b9f47e0e4d49d9441d17c67e50b841527
ARKye03 <rafa2003sanchez@gmail.com>
2024-04-06 15:55:13 -0400

    Remove mpd, waybar, wofi RIP Hyprland

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -9,39 +9,15 @@
   home.username = "nixarkye";
   home.homeDirectory = "/home/nixarkye";
 
-  home.stateVersion = "23.11"; # Please read the comment before changing.
+  home.stateVersion = "23.11";
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
   home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
   };
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. If you don't want to manage your shell through Home
-  # Manager then you have to manually source 'hm-session-vars.sh' located at
-  # either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/nixarkye/etc/profile.d/hm-session-vars.sh
-  #
   home.sessionPath = [
     "${config.home.homeDirectory}/.local/bin"
     "${config.home.homeDirectory}/.cargo/bin"
   ];
 
-  # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 }
