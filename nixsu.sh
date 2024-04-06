4e6ce5ef2a000ea74bc9320c7a7a7dc6b21cc6cb
ARKye03 <rafa2003sanchez@gmail.com>
2024-04-06 17:42:00 -0400

    Refactor nixsu.sh script to improve error handling and add commit generation information

--- nixsu.sh
+++ nixsu.sh
@@ -1,10 +1,18 @@
-#
 if [ $# -eq 0 ]; then
-    echo "Need generation name as argument"
-    exit 1
+  echo "Need generation name as argument"
+  exit 1
 fi
 
-echo "$1" > /home/nixarkye/.dotfiles/generation
-echo "$(date)" >> /home/nixarkye/.dotfiles/generation
-
 sudo nixos-rebuild switch
+
+if [ $? -ne 0 ]; then
+  echo "Error rebuilding system"
+  exit 1
+else
+  echo "System rebuilt successfully"
+  echo "$1" > /home/nixarkye/.dotfiles/generation
+  echo "$(date)" >> /home/nixarkye/.dotfiles/generation
+  
+  git add -A
+  git commit -m "New Generation: $1"
+fi
