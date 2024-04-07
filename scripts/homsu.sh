de55f312c1daa82651967f1cddb2e22451d68346
ARKye03 <rafa2003sanchez@gmail.com>
2024-04-07 14:48:43 -0400

    Add script to control version of home-manager

---
+++ scripts/homsu.sh
@@ -0,0 +1,22 @@
+$home_gen_name = "Home default"
+
+if [ $# -eq 0 ]; then
+  echo "No name gen specified"
+else
+  $home_gen_name = $1
+fi
+
+export NIXOS_LABEL="$home_gen_name"
+sudo home-manager switch --impure
+
+if [ $? -ne 0 ]; then
+  echo "Error rebuilding home"
+  exit 1
+else
+  echo "Home rebuilt successfully"
+  echo "$home_gen_name" > /home/nixarkye/.dotfiles/home-manager/home-generation
+  echo "$(date)" >> /home/nixarkye/.dotfiles/home-manager/home-generation
+  
+  git add -A
+  git commit -m "Home Generation: $home_gen_name"
+fi
