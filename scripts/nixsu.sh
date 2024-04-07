9271f9ad823b77c1a6bb9e478dfc6a747806c2d0
ARKye03 <rafa2003sanchez@gmail.com>
2024-04-07 14:49:21 -0400

    Revamped nixsu alias to control version of generations

--- scripts/nixsu.sh
+++ scripts/nixsu.sh
@@ -1,9 +1,12 @@
+$gen_name = "Default"
+
 if [ $# -eq 0 ]; then
-  echo "Need generation name as argument"
-  exit 1
+  echo "No name gen specified"
+else
+  $gen_name = $1
 fi
 
-export NIXOS_LABEL="$1"
+export NIXOS_LABEL="$gen_name"
 sudo nixos-rebuild switch --impure
 
 if [ $? -ne 0 ]; then
@@ -11,9 +14,9 @@ if [ $? -ne 0 ]; then
   exit 1
 else
   echo "System rebuilt successfully"
-  echo "$1" > /home/nixarkye/.dotfiles/generation
+  echo "$gen_name" > /home/nixarkye/.dotfiles/generation
   echo "$(date)" >> /home/nixarkye/.dotfiles/generation
   
   git add -A
-  git commit -m "New Generation: $1"
+  git commit -m "Nix Generation: $gen_name"
 fi
