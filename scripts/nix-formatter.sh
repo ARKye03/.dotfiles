9e2d12d0580cdb8a880f2053b62b7bca6edfd861
ARKye03 <rafa2003sanchez@gmail.com>
2024-04-07 14:51:52 -0400

    Remove comments and remove path to bash due to NixOS conflicts not fetching bash

--- scripts/nix-formatter.sh
+++ scripts/nix-formatter.sh
@@ -1,8 +1,4 @@
-#!/bin/bash
-
-# Loop through all files in the current directory and its subdirectories
 find . -name "*.nix" -type f | while read -r file; do
-    # Apply "nixpkgs-fmt" to the file
-    echo "Formatting: $file"
-    nixpkgs-fmt "$file"
+  echo "Formatting: $file"
+  nixpkgs-fmt "$file"
 done
