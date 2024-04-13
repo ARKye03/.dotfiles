a233593722730a09152dc9aa7e84b3a0118260be
ARKye03 <rafa2003sanchez@gmail.com>
2024-04-13 15:03:24 -0400

    Add cli utilities
    bat alternative to cat
    zoxide alternative to cd (z <path>)

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -18,14 +18,12 @@
     "${config.home.homeDirectory}/.cargo/bin"
     "${config.home.homeDirectory}/.venv/bin"
   ];
-  
-  programs ={
-  # Better `cd`
-  bat.enable = true;
-  # Type `z <pat>` to cd to some directory
-  zoxide.enable = true;
 
-  fzf.enable = true;
+  programs = {
+    bat.enable = true;
+    zoxide.enable = true;
+    fzf.enable = true;
 
-  home-manager.enable = true;};
+    home-manager.enable = true;
+  };
 }
