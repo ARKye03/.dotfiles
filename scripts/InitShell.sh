a129fab0c0cc4c4e4b4a98f6319984b9262b8b55
ARKye03 <rafa2003sanchez@gmail.com>
2024-04-20 11:55:28 -0400

    Add flakes to initshell.sh

--- scripts/InitShell.sh
+++ scripts/InitShell.sh
@@ -8,4 +8,20 @@ cat <<EOF > shell.nix
         nativeBuildInputs = with pkgs.buildPackages; [ $@ ];
     }
 EOF
+cat <<EOF > flake.nix
+{
+  description = "my project description";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let pkgs = nixpkgs.legacyPackages.\${system}; in
+        {
+          devShells.default = import ./shell.nix { inherit pkgs; };
+        }
+      );
+}
+EOF
 fi
