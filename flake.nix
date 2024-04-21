8b8f46bd43baec5a6a3adf0f7d0d3c79c61314bf
ARKye03 <rafa2003sanchez@gmail.com>
2024-04-21 18:55:05 -0400

    Add links to sys.nixes

--- flake.nix
+++ flake.nix
@@ -1,10 +1,1 @@
-{
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
-  outputs = { self, nixpkgs }: {
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [ ./configuration.nix ];
-    };
-  };
-}
+/etc/nixos/flake.nix
