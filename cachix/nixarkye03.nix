c7ef900d2f16b3baf04a894eec3f5861a6f473cd
ARKye03 <rafa2003sanchez@gmail.com>
2024-04-21 22:00:34 -0400

    Modified types
    Now the system is controlled from here and nowhere else

---
+++ cachix/nixarkye03.nix
@@ -0,0 +1,13 @@
+
+{
+  nix = {
+    settings = {
+      substituters = [
+        "https://nixarkye03.cachix.org"
+      ];
+      trusted-public-keys = [
+        "nixarkye03.cachix.org-1:LfH970Q+ftI3HaWzBeERfpDBqcS+olFQOv6O4tiqM/A="
+      ];
+    };
+  };
+}
--- flake.nix
+++ flake.nix
@@ -1,1 +1,10 @@
-/etc/nixos/flake.nix
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [ ./configuration.nix ];
+    };
+  };
+}
