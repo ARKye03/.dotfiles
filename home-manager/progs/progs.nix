b33c32f65c287b97e904dac76e8de543e3021bbc
ARKye03 <rafa2003sanchez@gmail.com>
2024-05-01 19:37:31 -0400

    Update home-manager/progs/progs.nix

--- home-manager/progs/progs.nix
+++ home-manager/progs/progs.nix
@@ -2,9 +2,7 @@
   nixpkgs.config.allowUnfree = true;
 
   programs = {
-    bat.enable = true;
     ripgrep.enable = true;
-    zoxide.enable = true;
     fd.enable = true;
     firefox.enable = true;
     neovim.enable = true;
