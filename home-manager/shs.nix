f6bfb7733c743f880fc2b6bb99f003596e11e686
ARKye03 <rafa2003sanchez@gmail.com>
2024-05-20 17:08:25 -0400

    Home Generation: Home default

--- home-manager/shs.nix
+++ home-manager/shs.nix
@@ -91,7 +91,6 @@ in
           #ZStyles Completions
           zstyle ':completion:*' verbose true
           zstyle ':completion:*:*:*:*:*' menu select
-          zstyle ':completion:*' matcher-list '\' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
 
         bindkey '^[[1;5C' forward-word  # Ctrl + Right
         bindkey '^[[1;5D' backward-word  # Ctrl + Left
