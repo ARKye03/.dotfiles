e663caa85977f387d3dfbd95e2f204e2406e019f
ARKye03 <rafa2003sanchez@gmail.com>
2024-06-06 09:52:56 -0400

    Home Generation: Migrate to shell script for home generation

---
+++ scripts/homsu.sh
@@ -0,0 +1,23 @@
+# shellcheck disable=SC2148
+home_gen_name="Home default"
+
+if [ $# -eq 0 ]; then
+  echo "No name gen specified"
+else
+  home_gen_name=$1
+fi
+
+pushd ~/.dotfiles/home-manager/ || exit
+
+if ! (home-manager switch --flake ../); then
+  echo "Error rebuilding home"
+  exit 1
+else
+  echo "Home rebuilt successfully"
+  echo "$home_gen_name" > home-generation
+  date >> home-generation
+  
+  git add .
+  git commit -m "Home Generation: $home_gen_name"
+  popd || exit
+fi
